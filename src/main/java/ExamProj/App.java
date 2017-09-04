37eaae8bd2607051e67c9401ae332848eacf2ad2
RainbowG26 <lem.feather@yandex.ua>
2017-09-04 17:07:30 +0300

    First commit 040917

---
+++ src/main/java/ExamProj/App.java
@@ -0,0 +1,13 @@
+package ExamProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
